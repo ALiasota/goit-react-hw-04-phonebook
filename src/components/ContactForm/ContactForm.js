dcecbb24079c9d97f3b84c0c2ba65bf56be06d2b
Andrii <Andrii@iMac.local>
2022-04-29 15:50:51 +0300

    pravku

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,33 +1,32 @@
 import React, { useState } from 'react';
-import styles from './ContactForm.module.css'
+import styles from './ContactForm.module.css';
 import propTypes from 'prop-types';
 
-const ContactForm = ({onSubmit}) => {
-    
-const [name, setName] = useState('');
-const [number, setNumber] = useState('');
+const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-const handleSubmit = e =>  {
-  e.preventDefault();
-  onSubmit({name, number});  
-  setName('');
-  setNumber('');  
-}
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSubmit({ name, number });
+    setName('');
+    setNumber('');
+  };
 
-const handleChangeName = (e) => {
-  setName(e.target.value);   
-};
+  const handleChangeName = e => {
+    setName(e.target.value);
+  };
 
-const handleChangeNumber = (e) => {  
-  setNumber(e.target.value);  
-};
+  const handleChangeNumber = e => {
+    setNumber(e.target.value);
+  };
 
-  return(
-     <form className={styles.ContactForm} onSubmit={handleSubmit}>
-        <h1>Phonebook</h1>
-        <label className={styles.label}>
+  return (
+    <form className={styles.ContactForm} onSubmit={handleSubmit}>
+      <h1>Phonebook</h1>
+      <label className={styles.label}>
         Name
-          <input
+        <input
           className={styles.inp}
           type="text"
           name="name"
@@ -36,11 +35,11 @@ const handleChangeNumber = (e) => {
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
-          />
-        </label> 
-        <label className={styles.label}>
+        />
+      </label>
+      <label className={styles.label}>
         Number
-          <input
+        <input
           className={styles.inp}
           type="tel"
           name="number"
@@ -49,24 +48,17 @@ const handleChangeNumber = (e) => {
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
-          />
-        </label> 
-        <button
-        className={styles.btn}
-        type="submit">        
-          Add contact
-        </button>
-      </form>
-  )
-
-}
+        />
+      </label>
+      <button className={styles.btn} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+};
 
 ContactForm.propTypes = {
   onSubmit: propTypes.func.isRequired,
-  
-}
-    
-   
-
+};
 
 export default ContactForm;
--- src/components/ContactsList/ContactsList.js
+++ src/components/ContactsList/ContactsList.js
@@ -1,22 +1,33 @@
-import Contact from "../Contact";
+import ContactsListItem from '../ContactsListItem';
 import propTypes from 'prop-types';
 
-const ContactsList = ({contacts, onDelete}) => {
-    return(
-        <ul>
-          {contacts.map(contact =>{              
-            return <Contact key={contact.id} contact={contact} onDelete={onDelete}/>
-          })}
-        </ul>
-    )
-}
+const ContactsList = ({ contacts, onDelete }) => {
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) => {
+        return (
+          <ContactsListItem
+            key={id}
+            id={id}
+            name={name}
+            number={number}
+            onDelete={onDelete}
+          />
+        );
+      })}
+    </ul>
+  );
+};
 
 ContactsList.propTypes = {
   onDelete: propTypes.func.isRequired,
   contacts: propTypes.arrayOf(
-    propTypes.object
-  ).isRequired
-      
-}
+    propTypes.shape({
+      id: propTypes.string.isRequired,
+      name: propTypes.string.isRequired,
+      number: propTypes.string.isRequired,
+    })
+  ).isRequired,
+};
 
 export default ContactsList;
